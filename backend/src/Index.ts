6bf3abde5a0fb076586cdd06e185f4dcfa498096
femi abimbola <femiabimbola6@gmail.com>
2025-04-21 04:04:19 -0700

    wrote the schema

--- backend/src/Index.ts
+++ backend/src/Index.ts
@@ -1,6 +1,9 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectToMongoose } from "./database/config";
+import { corsOptions, encodedOptions } from "./utils/options";
+
 
 dotenv.config();
 
@@ -11,8 +14,11 @@ app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Express with TypeScript Server");
 });
 
+
+
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded(encodedOptions))
+app.use(cors(corsOptions))
 
 app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
---
+++ backend/src/utils/options.ts
@@ -0,0 +1,9 @@
+export const corsOptions = {
+  origin : ['*'],
+  credential: true //Majorly because of cookies
+}
+
+export const encodedOptions = {
+  limit: "100mb",
+  extended:true
+}
