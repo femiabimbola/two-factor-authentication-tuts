ff79f28139a8351997d60ad783cb631ebf414e58
femi abimbola <femiabimbola6@gmail.com>
2025-05-07 22:51:39 -0700

    added the component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const  Home = () => {
   return ( 
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-3xl font-bold text-white"> A 2 Factor Authentication</h1>npx shadcn@latest init
+      <h1 className="text-3xl font-bold text-white"> A 2 Factor Authentication</h1>
 
-      <p className="text-white">A Two Factor Authentication with Speakeasy and Jsonwebtoken</p>
+      <p className="text-white pb-3">A Two Factor Authentication with Speakeasy and Jsonwebtoken</p>
+      <Button className="bg-white text-blue-500" asChild>
+        <Link href={'/login'}> Login  </Link>        
+        </Button>
     </div>
     
   );
