64840080a85f8ed8b62c01d2b9697010c98285a0
femi abimbola <femiabimbola6@gmail.com>
2025-05-09 23:46:37 -0700

    setting up the component

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -1,10 +1,41 @@
-"use client"
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const LoginSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    
+  }
+
   return (
     <div>
       <p> Login Form</p>
     </div>
-  )
-}
-
+  );
+};
