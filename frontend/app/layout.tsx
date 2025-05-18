679e65ca7f721ad655996d3958e25c8d7a2e20f4
femi abimbola <femiabimbola6@gmail.com>
2025-05-17 23:14:53 -0700

    working on the login

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SessionProvider } from "@/context/SessionContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="bg-slate-900 h-screen">
-          {children}
+          <SessionProvider>
+            {children}
+          </SessionProvider>
         </main>
       </body>
     </html>
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,10 @@
 import { LoginForm } from "@/components/LoginForm"
+import { useSession } from "@/context/SessionContext"
+
 
 const Login = () => {
+// const {login} = useSession()
+const handleLoginSuccess = () => {}
   return (
    <LoginForm />
   )
