fc819fa6ccf3d42fa2aa5e21e80d46f87e0620b8
femi abimbola <femiabimbola6@gmail.com>
2025-05-20 11:31:24 -0700

    setting up the two FA

--- frontend/components/TwoFASetup.tsx
+++ frontend/components/TwoFASetup.tsx
@@ -1,5 +1,19 @@
+import CardWrapper from "./CardWrapper"
+
 export const TwoFASetup = () => {
   return(
-    <div> The Two FA</div>
+    <div className="flex justify-center items-center h-full">
+      <CardWrapper
+        label="Have an account? Sign in"
+        Opplink="/login"
+      >
+        <h1> Turn Two Factor Authentication</h1>
+        <p>Scan the authenticator Application</p>
+        <div className="">
+
+        </div>
+      </CardWrapper>
+    </div>
+    
   )
 }
